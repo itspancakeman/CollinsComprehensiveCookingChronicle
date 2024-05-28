63c86431958738cdf6dbd5b30ff8e0f127200e91
itspancakeman <collinjst.onge@gmail.com>
2024-05-28 11:49:16 -0400

    feat: established connection to database

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI);
+
+const db = mongoose.connection;
+
+db.once('open', () => console.log(`Connected to MongoDB at ${db.host}:${db.port}`));
+db.on('error', (error) => console.log('Database error\n', error));
+
+const User = require('./user');
+
+module.exports = {
+    User,
+}
