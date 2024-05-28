d26541d464aa3d8353615eaa9e5a6b7f94ace9b2
itspancakeman <collinjst.onge@gmail.com>
2024-05-28 11:21:45 -0400

    feat: added passport config to verify user information

---
+++ config/passport-config.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+// const { validPassword } = require('../utils');
+// const { User } = require('../models');
+
+const STRATEGY = new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+}, async (username, password, callback) => {
+    try {
+        const user = await User.findOne({ username });
+
+        if (!user || !validPassword(password, user.password)) {
+            callback(null, false);
+        } else {
+            callback(null, user);
+        }
+    } catch (error) {
+        console.log('----error-----\n', error);
+    }
+});
+
+passport.serializeUser((user, callback) => {
+    callback(null, user.email);
+});
+
+passport.deserializeUser(async (email, callback) => {
+    try {
+        const user = await User.findOne({ email });
+
+        if (user) {
+            callback(null, user);
+        }
+    } catch (error) {
+        console.log('-----error in passport config-----\n', error);
+    }
+});
+
+passport.use(STRATEGY);
+module.exports = passport;
